3c9c356ce6be591adf23624d0c330a96e196adc6
FloranteAtencio <atencioflorantejr@gmail.com>
2024-12-18 18:31:49 +0800

    Full Development Copy and Pasting

--- full_backup.sh
+++ full_backup.sh
@@ -4,14 +4,14 @@ export ORACLE_HOME=/opt/oracle/product/23ai/dbhomeFree
 
 rman target / <<EOF
 RUN {
-  BACKUP DATABASE PLUS ARCHIVELOG FORMAT '/Home/Oracle/full_backup_%d_%T';
-  BACKUP PLUGGABLE DATABASE ALL FORMAT '/Home/Oracle/pluggable_full_backup_%d_%T'
+  BACKUP DATABASE PLUS ARCHIVELOG FORMAT '/home/oracle/backup/full_backup_%d_%T';
+  BACKUP PLUGGABLE DATABASE ALL FORMAT '/home/oracle/backup/pluggable_full_backup_%d_%T'
     FILE_NAME_CONVERT=('/opt/oracle/oradata/FREE', '/Home/Oracle/Pluggable/FREE');
   DELETE OBSOLETE;
 }
 EOF
 
 # Remove backups older than 30 days
-find "/Home/Oracle" -name "full_backup_*" -type f -mtime +30 -exec rm {} \;
-find "/Home/Oracle" -name "pluggable_full_backup_*" -type f -mtime +30 -exec rm {} \;
+find "/home/oracle/backup" -name "full_backup_*" -type f -mtime +30 -exec rm {} \;
+find "/home/oracle/backup" -name "pluggable_full_backup_*" -type f -mtime +30 -exec rm {} \;
 # backup shell file located in /Home/Oracle/Script/
